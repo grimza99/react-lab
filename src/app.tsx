67edc0f479f036960ab50325bd3015f8b767be5b
grimza99 <gbtmxlf@naver.com>
2025-10-07 18:31:30 +0900

    feat: 리소스 캐시 및 Suspense 컴포넌트 추가

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,24 @@
 const myAppState = [];
 /** 배열에 접근할 커서 */
 let myAppStateCursor = 0;
+/** 리소스 캐시 */
+const resourceCache = {};
+/** 사진 URL 생성기 */
+const photoURL = (seed) =>
+  `https://picsum.photos/seed/${encodeURIComponent(seed)}/200`;
 
 const React = {
   createElement: (tag, props, ...children) => {
     if (typeof tag === "function") {
-      return tag(props, ...children);
+      try {
+        return tag(props, ...children);
+      } catch ({ promise, key, fallback }) {
+        promise.then((value) => {
+          resourceCache[key] = value;
+          reRender();
+        });
+        return fallback;
+      }
     }
     const el = {
       tag,
@@ -67,10 +80,29 @@ const useState = (initialState) => {
   return [myAppState[stateCursor], setState];
 };
 
+const createResource = (asyncTask, key, fallback) => {
+  if (resourceCache[key]) return resourceCache[key];
+  const promise = asyncTask();
+  throw { promise, key, fallback };
+};
+const getMyAwesomePic = (seed) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(photoURL(seed)), 1500);
+  });
+};
+
+const Suspense = (props, children) => {
+  const { fallback, key, task } = props;
+  createResource(task, key, fallback);
+  return children;
+};
 /** 어플리케이션 */
 const App = () => {
   const [name, setName] = useState("react");
   const [count, setCount] = useState(0);
+  const photo1 = resourceCache["photo1"];
+  const photo2 = resourceCache["photo2"];
+
   return (
     <div draggable>
       <h2>Hello {name}!</h2>
@@ -83,6 +115,21 @@ const App = () => {
       <h2> 카운터 값: {count}</h2>
       <button onclick={() => setCount(count + 1)}>+1</button>
       <button onclick={() => setCount(count - 1)}>-1</button>
+      <h2>저희 사진 앨범</h2>
+      <Suspense
+        fallback={<h2>사진1 로딩중...</h2>}
+        task={() => getMyAwesomePic("photo1")}
+        key={"photo1"}
+      >
+        <img src={photo1} alt="사진1" />
+      </Suspense>
+      <Suspense
+        fallback={<h2>사진2 로딩중...</h2>}
+        task={() => getMyAwesomePic("photo2")}
+        key={"photo2"}
+      >
+        <img src={photo2} alt="사진2" />
+      </Suspense>
     </div>
   );
 };
